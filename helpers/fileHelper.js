614388aa00549be94c002eb96ab90fd10c864a85
Igor Subbotin <moat1981@gmail.com>
2016-07-13 18:53:26 +0000

    Remove redundant console logging from fileHelper.js

--- helpers/fileHelper.js
+++ helpers/fileHelper.js
@@ -3,12 +3,10 @@ var path = require("path");
 var fileHelper = function()
 {
     var getExtension = function(fileName) {
-        console.log(fileName);
         var extension = path.extname(fileName);
         var basename = path.basename(fileName, extension);
         extension = path.extname(basename) + extension;
         extension = extension.substring(1);
-        console.log(extension);
         return extension;
     };
     
